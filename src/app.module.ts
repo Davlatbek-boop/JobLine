b738c82aa83937ab302507f6e6fea2115c948bd1
Davlatbek <salimovdavlat0526@.gmail.com>
2025-08-06 11:21:47 +0500

    default sozlamalar to'g'irlandi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      autoLoadEntities: true,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,55 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { BadRequestException, ValidationPipe } from "@nestjs/common";
+import * as cookieParser from "cookie-parser";
+import { WinstonModule } from "nest-winston";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+async function start() {
+  const PORT = process.env.PORT ?? 5000;
+  const app = await NestFactory.create(AppModule, {
+    // logger: WinstonModule.createLogger(winstonConfig),
+  });
+
+  // app.useGlobalFilters(new AllExseptionsFilter())
+
+  app.use(cookieParser());
+
+  // app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix("api");
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigin = [
+        "http://18.153.199.72:3003",
+        "http://localhost:3004",
+        "http://localhost:8000",
+      ];
+      if (!origin || allowedOrigin.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new BadRequestException("Not allowed by CORS"));
+      }
+    },
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle("Job Line")
+    .setDescription("Job Line REST API")
+    .setVersion("1.0")
+    .addTag("NestJS, CRUD, swagger, sendMail, tokens, Validation")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, document);
+
+  await app.listen(PORT, () => {
+    console.log(`Server started at: http://localhost:${PORT}`);
+  });
 }
-bootstrap();
+start();
